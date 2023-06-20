9af43de9a894eaf236b8d1d10b6f6becdcc7bbf5
rabobahago <rabobahago@gmail.com>
2023-06-20 17:43:08 +0100

    Question component implemented

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,10 @@ import { useEffect, useReducer } from "react";
 import Header from "./Header.jsx";
 import Home from "./Home.jsx";
 import Loader from "./Loader.jsx";
-import StartScreen from "./StartScreen.jsx";
-import Question from "./Question.jsx";
 const initialState = {
   questions: [],
   //loading, error, ready, finished
   status: "loading",
-  index: 0,
 };
 function reducer(state, action) {
   switch (action.type) {
@@ -23,21 +20,12 @@ function reducer(state, action) {
         ...state,
         status: "error",
       };
-    case "start":
-      return {
-        ...state,
-        status: "active",
-      };
     default:
       throw new Error("Action unknown");
   }
 }
 const App = () => {
-  const [{ questions, status, index }, dispatch] = useReducer(
-    reducer,
-    initialState
-  );
-  let numQuestions = questions.length;
+  const [{ questions, status }, dispatch] = useReducer(reducer, initialState);
   useEffect(function () {
     fetch("http://localhost:8000/questions")
       .then((res) => res.json())
@@ -50,10 +38,8 @@ const App = () => {
       <Home className="main">
         {status === "loading" && <Loader />}
         {status === "error" && <Error />}
-        {status === "ready" && (
-          <StartScreen numQuestions={numQuestions} dispatch={dispatch} />
-        )}
-        {status === "active" && <Question quest={questions[index]} />}
+        <p>1/16</p>
+        <p>Question?</p>
       </Home>
     </div>
   );
